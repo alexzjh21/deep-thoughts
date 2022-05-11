b42ca94bdc8b5ab55fe4de0df39e5301d40d44b8
alexzjh21 <alexzjh21@yahoo.co.jp>
2022-05-11 15:13:54 -0400

    handle changes on thoughts

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -11,11 +11,26 @@ export const LOGIN_USER = gql`
     }
   }
 `;
+
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
       token
       user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_FRIEND = gql`
+  mutation addFriend($id: ID!) {
+    addFriend(friendId: $id) {
+      _id
+      username
+      friendCount
+      friends {
         _id
         username
       }
